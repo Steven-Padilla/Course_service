ecbb5f0ea0f3fb45df44392569ac83ff2850bb9a
Steven-Padilla <steven_padilla728@outlook.com>
2023-06-27 20:08:13 -0600

    fix: RESTFUL API implemented and docker image tag updated

--- src/course/infrastructure/CourseRouter.ts
+++ src/course/infrastructure/CourseRouter.ts
@@ -6,12 +6,12 @@ import { createCourseController } from "./dependencies/CreateCourseDependency";
 import { deleteCourseController } from "./dependencies/DeleteCourseDependency";
 import { updateCourseController } from "./dependencies/UpdateCourseDependency";
 
-courseRouter.get("/getall/",getAllCoursesController.run.bind(getAllCoursesController)
+courseRouter.get("/courses/",getAllCoursesController.run.bind(getAllCoursesController)
 );
-courseRouter.post("/create/",createCourseController.run.bind(createCourseController)
+courseRouter.post("/courses/",createCourseController.run.bind(createCourseController)
 );
-courseRouter.put("/update/", updateCourseController.run.bind(updateCourseController));
-courseRouter.delete("/delete/:id",deleteCourseController.run.bind(deleteCourseController)
+courseRouter.put("/courses/", updateCourseController.run.bind(updateCourseController));
+courseRouter.delete("/courses/:id",deleteCourseController.run.bind(deleteCourseController)
 );
 
 export { courseRouter };
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.use(bodyParser.json());
-app.use("/course/", courseRouter)
+app.use("/api/v1", courseRouter)
 app.listen(port, () => {
     console.log(`[APP] - Starting application on port ${port}`);
 });
